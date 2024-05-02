ac78750c749b514146aa06847a3501a30adaf6d3
Eric106 <ercv106@hotmail.com>
2024-05-02 09:40:17 -0600

    Thu 02 May 2024 09:40:17 AM CST

--- ddns-cloudflare.sh
+++ ddns-cloudflare.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
 cd "$(dirname "$0")"
-./ddns-cloudflare
+# Detectar la arquitectura del sistema
+arch=$(uname -m)
+
+# Verificar la arquitectura y ejecutar el comando correspondiente
+if [[ $arch == "aarch64"* ]]; then
+    bin/ddns-cloudflare_aarch64
+
+elif [[ $arch == "x86_64" || $arch == "amd64" ]]; then
+    bin/ddns-cloudflare_x86_64
+fi
--- make.sh
+++ make.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+cd "$(dirname "$0")"
 pyinstaller --onefile ddns-cloudflare.py -y
-mv dist/ddns-cloudflare ./
-rm dist/ -rf
+
+# Detectar la arquitectura del sistema
+arch=$(uname -m)
+
+# Verificar la arquitectura y ejecutar el comando correspondiente
+if [[ $arch == "aarch64"* ]]; then
+    echo "Binary for ARM64 complied"
+    mv dist/ddns-cloudflare bin/ddns-cloudflare_aarch64
+
+elif [[ $arch == "x86_64" || $arch == "amd64" ]]; then
+    echo "Binary for x86_64 complied"
+    mv dist/ddns-cloudflare bin/ddns-cloudflare_x86_64
+fi
+
+rm dist/ -rf ; rm build/ -rf ; rm ddns-cloudflare.spec
